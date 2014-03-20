d99b3d790ca2dc44ea77f73d7a1f776c7dad3646
rikki cattermole <pbdadmin@gmail.com>
2014-03-21 00:42:42 +1300

    Loki, please change the values are you need. Its just what I need to get started (it won't break anything my end).

--- interface/zimp/engine/defs.py
+++ interface/zimp/engine/defs.py
@@ -5,3 +5,14 @@ class Direction:
     DOWN = 1 << 2
     LEFT = 1 << 3
     RIGHT = 1 << 4
+
+
+class Tiles:
+    Foyer = 0
+    Patio = 1
+    EvilTemple = 2
+    StorageRoom = 3
+    Kitchen = 4
+    DiningRoom = 5
+    Garden = 6
+    Graveyard = 7
